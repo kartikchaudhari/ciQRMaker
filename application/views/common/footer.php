3748b3d2a20e5b7106d728765ddb3f4adbd4953e
kartikchaudhari <kartikchaudhari456@gmail.com>
2019-04-14 19:27:25 +0530

    Initial Commit

--- application/views/common/footer.php
+++
@@ -1,6 +0,0 @@
-
-<!--  bootstrap javascripts-->
-<script type="text/javascript" src="<?php echo base_url(JQUERY); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url(BOOTSTRAP_JS); ?>"></script>
-</body>
-</html>	
